ed5fb3df624fcc2caac1ce7b54e652f9a8b2fdaf
GOKSIK <91minjung@gmail.com>
2024-06-28 20:43:33 +0900

    로그인페이지 조금 고침

--- src/app/(user)/login/page.tsx
+++ src/app/(user)/login/page.tsx
@@ -1,12 +1,17 @@
+import Image from "next/image";
+import Link from "next/link";
 export default function Login(){
   return(
-    <div>
-      <div className="login-logo">
-        <img src="/logo.png" className="login-img"/>
-        <p className="login-text">로그인</p>
-      </div>
-      <div>
-        <textarea>id</textarea>
+    <div className="flex flex-col justify-center items-center">
+      <div className="py-10">
+        <Image src="/img/logo_binary.svg" alt="Hourglass Logo" width={300} height={300}></Image>
+        <div className="flex flex-row">
+          <div className="flex flex-col">
+            <input className="my-2" type="email" name="email" id="" />
+            <input className="" type="password" name="password" id="" />
+          </div>
+          <button className="login-text px-4 bg-orange-200 rounded">로그인</button>
+        </div>
       </div>
     </div>
   )
