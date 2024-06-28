7cc6fecfd4772eb40e4cf9d0989ee56c846b60bb
구자건 <wkrjsdl5632@gmail.com>
2024-06-28 13:31:46 +0900

    feat: userservice init

---
+++ src/app/(user)/login/page.tsx
@@ -0,0 +1,10 @@
+import Header from "@/app/(user)/header-user-service";
+
+export default function Login(){
+  return(
+    <div>
+      <Header></Header>
+      <textarea>id</textarea>
+    </div>
+  )
+}
