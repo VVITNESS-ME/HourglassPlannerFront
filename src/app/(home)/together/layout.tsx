a8eb9670de051c9d2d3ab3f180f5273f1bc5a034
김회일 <klasrptmxm@gmail.com>
2024-07-08 17:07:50 +0900

    feat : 같이하기 layout.tsx 파일추가 -> 좌우반전 문제 해결

---
+++ src/app/(home)/together/layout.tsx
@@ -0,0 +1,10 @@
+import RootLayout from "@/components/together/facelandmark-demo/app/layout";
+
+export default function TogetherLayout({ children }: Readonly<{ children: React.ReactNode;}>) {
+  return (
+    <div className="">
+        {children}
+        <RootLayout />
+    </div>
+  );
+}
