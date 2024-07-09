81035b00599a8f522e146f663b2c4626ba397afb
구자건 <wkrjsdl5632@gmail.com>
2024-07-09 20:26:32 +0900

    feat: middle merge

--- store/(auth)/jwtDecode.ts
+++ store/(auth)/jwtDecode.ts
@@ -1,4 +1,10 @@
-import { jwtDecode, JwtPayload } from 'jwt-decode';
-export const decodeJwt = (token: string): JwtPayload => {
-  return jwtDecode<JwtPayload>(token);
+import { JwtPayload, jwtDecode } from 'jwt-decode';
+
+interface ExtendedJwtPayload extends JwtPayload {
+  email: string;
+  sub: string;
+}
+
+export const decodeJwt = (token: string): ExtendedJwtPayload => {
+  return jwtDecode<ExtendedJwtPayload>(token);
 };
