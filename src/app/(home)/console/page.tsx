e24558834d0084c1d648e17be6f0ad737d47bfbf
구자건 <wkrjsdl5632@gmail.com>
2024-07-09 19:28:50 +0900

    Feat: 콘솔창 레이아웃 수정

--- src/app/(home)/console/page.tsx
+++ src/app/(home)/console/page.tsx
@@ -14,24 +14,24 @@ const MainConsole: React.FC = () => {
   return (
     <div className="min-h-screen p-8 bg-gray-100">
       <div className="flex flex-col lg:flex-row gap-4 flex-wrap">
-        <div className="flex-1 min-w-[400px] max-w-[700px] max-h-[500px] mb-4">
+        <div className="flex-1 min-w-[400px] max-w-[700px] h-[650px] mb-4">
           <Calendar/>
         </div>
-        <div className="flex-1 min-w-[400px] max-w-[700px] max-h-[500px] mb-4">
+        <div className="flex-1 min-w-[400px] max-w-[700px] h-[650px] mb-4">
           <TodayTasks/>
         </div>
-        <div className="flex-1 min-w-[400px] max-w-[700px] max-h-[500px] mb-4">
+        <div className="flex-1 min-w-[400px] max-w-[700px] h-[650px] mb-4">
           <Hourglass/>
         </div>
       </ div>
       <div className="flex flex-col lg:flex-row gap-4 flex-wrap">
-        <div className="flex-1 min-w-[400px] max-w-[700px] max-h-[500px] mb-4">
+        <div className="flex-1 min-w-[400px] max-w-[700px] max-h-[700px] mb-4">
           <SandTimerTasks/>
         </div>
-        <div className="flex-1 min-w-[400px] max-w-[700px] max-h-[500px] mb-4">
+        <div className="flex-1 min-w-[400px] max-w-[700px] max-h-[700px] mb-4">
           <CompletedTasks/>
         </div>
-        <div className="flex-1 min-w-[400px] max-w-[700px] max-h-[500px] mb-4">
+        <div className="flex-1 min-w-[400px] max-w-[700px] max-h-[700px] mb-4">
           <AchievementCard/>
         </div>
       </ div>
