1419b6821e8b932626e6cdcacd333c167647ae4b
PenLoo98 <sohnchanho@gmail.com>
2024-07-20 00:53:04 +0900

    feat: 채팅방 유저수 변동사항 -> API 서버에게 알림

--- server.js
+++ server.js
@@ -29,6 +29,30 @@ app.prepare().then(() => {
   // 방 정보 저장
   const rooms = new Map();
 
+  // Backend 서버에 인원 수 변동 fetch
+  const fetchParticipants = async (roomId, roomSize) => {
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/together/participants/${roomId}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ current: roomSize }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      console.log(`방 ${roomId}의 인원 수: ${roomSize}`);
+    } catch (error) {
+      console.error("Fetch error:", error);
+    }
+  };
+
   // 방에 접속한 유저 수를 응답하는 API 엔드포인트 추가
   server.get("/room/:roomId/users", (req, res) => {
     const roomId = req.params.roomId;
@@ -54,17 +78,20 @@ app.prepare().then(() => {
   });
 
   io.on("connection", (socket) => {
-    socket.on("join", (roomId) => {
+    socket.on("join", async (roomId) => {
       socket.join(roomId);
       const room = rooms.get(roomId) || new Set();
       room.add(socket.id);
       rooms.set(roomId, room);
 
-      // Send the list of users to the newly joined user
+      // 새로운 들어온 유저에게 해당 방에 있는 유저 목록을 전송
       socket.emit("users", Array.from(room));
 
-      // Notify others in the room that a new user has joined
+      // 해당 방에 접속 중인 유저들에게 새로운 유저가 접속했음을 알림
       socket.to(roomId).emit("userJoined", socket.id);
+
+      // 인원 수 변동 fetch
+      await fetchParticipants(roomId, room.size);
     });
 
     socket.on("offer", ({ roomId, toUserId, offer }) => {
@@ -79,7 +106,7 @@ app.prepare().then(() => {
       socket.to(userId).emit("candidate", { fromUserId: socket.id, candidate });
     });
 
-    socket.on("disconnecting", () => {
+    socket.on("disconnecting", async () => {
       for (const room of socket.rooms) {
         if (rooms.has(room)) {
           const users = rooms.get(room);
@@ -89,6 +116,8 @@ app.prepare().then(() => {
           } else {
             socket.to(room).emit("userLeft", socket.id);
           }
+          // 인원 수 변동 fetch
+          await fetchParticipants(room, users.size);
         }
       }
     });
