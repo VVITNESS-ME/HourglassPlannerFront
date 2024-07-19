f37b1705875b0b012bcb71ffc8183e21feb3b055
구자건 <wkrjsdl5632@gmail.com>
2024-07-19 11:11:12 +0900

    Feat: 타이틀 불러오기/ 설정 api 호출부 작성 (1/3)

--- src/app/(home)/mypage/profile/page.tsx
+++ src/app/(home)/mypage/profile/page.tsx
@@ -7,10 +7,10 @@ import CategorySettings from "@/components/mypage/profile/categorySettings";
 import useAuthStore from "../../../../../store/(auth)/authStore";
 
 interface Title {
-  titleId: number;
+  id: number;
   name: string;
-  description: string;
-  color: string;
+  achieveCondition: string;
+  titleColor: string;
 }
 
 interface UserInfo {
@@ -91,37 +91,38 @@ const Profile: React.FC = () => {
     }
   };
 
+  const fetchTitleInfo = async () => {
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/title`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: 'include',
+      });
+      const data = await response.json();
+      console.log(data);
+      if (response.ok) {
+        setTitles(data.data.achievedTitleList);
+      }
+    } catch (error) {
+      console.error('Error fetching profile info', error);
+    }
+  };
+
   useEffect(() => {
     initialize();
   }, [initialize]);
 
   useEffect(() => {
+    fetchTitleInfo();
+    fetchCategoriesInfo();
     if (isInitialized) {
       setUserInfo({
         userEmail: email,
         userName: username,
         main_title: 1,
       });
-
-      const testTitleData = [
-        { titleId: 1, name: '망부석', description: '3시간 동안 자리이탈/졸음 없음', color: '#228B22' },
-        { titleId: 2, name: '원펀맨', description: '운동 카테고리 100시간 달성', color: '#1E90FF' },
-        { titleId: 3, name: '전 집중 호흡', description: '1시간 동안 자리이탈/졸음 없음', color: '#8A2BE2' },
-        { titleId: 4, name: '시작이 반', description: '10분 모래시계 완료', color: '#FFD700' },
-        { titleId: 5, name: '그건 제 잔상입니다만', description: '30분 이내 10회 이상 자리이탈/졸음', color: '#FF69B4' },
-        { titleId: 6, name: '망부석', description: '3시간 동안 자리이탈/졸음 없음', color: '#228B22' },
-        { titleId: 7, name: '원펀맨', description: '운동 카테고리 100시간 달성', color: '#1E90FF' },
-        { titleId: 8, name: '전 집중 호흡', description: '1시간 동안 자리이탈/졸음 없음', color: '#8A2BE2' },
-        { titleId: 9, name: '시작이 반', description: '10분 모래시계 완료', color: '#FFD700' },
-        { titleId: 10, name: '그건 제 잔상입니다만', description: '30분 이내 10회 이상 자리이탈/졸음', color: '#FF69B4' },
-        { titleId: 11, name: '망부석', description: '3시간 동안 자리이탈/졸음 없음', color: '#228B22' },
-        { titleId: 12, name: '원펀맨', description: '운동 카테고리 100시간 달성', color: '#1E90FF' },
-        { titleId: 13, name: '전 집중 호흡', description: '1시간 동안 자리이탈/졸음 없음', color: '#8A2BE2' },
-        { titleId: 14, name: '시작이 반', description: '10분 모래시계 완료', color: '#FFD700' },
-        { titleId: 15, name: '그건 제 잔상입니다만', description: '30분 이내 10회 이상 자리이탈/졸음', color: '#FF69B4' },
-      ];
-      setTitles(testTitleData);
-      fetchCategoriesInfo();
     }
   }, [isInitialized, email, username]);
 
--- src/components/mypage/profile/titleList.tsx
+++ src/components/mypage/profile/titleList.tsx
@@ -5,10 +5,10 @@ import CardLayout from '../../cardLayout';
 import Button from './button';
 
 interface Title {
-  titleId: number;
+  id: number;
   name: string;
-  description: string;
-  color: string;
+  achieveCondition: string;
+  titleColor: string;
 }
 
 interface UserInfo {
@@ -26,15 +26,20 @@ const TitleList: React.FC<TitleListProps> = ({ titles, setUserInfo }) => {
   const [selectedTitle, setSelectedTitle] = useState<number | null>(null);
 
   const handleSelectTitle = async () => {
-    if (selectedTitle === null) return;
-
-    const selectedTitleId = titles[selectedTitle].titleId;
+    let selectedTitleId;
+    if (selectedTitle !== null){
+      selectedTitleId = titles[selectedTitle].id;
+    }else{
+      selectedTitleId = 0;
+    }
+    console.log(selectedTitleId);
     try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/profile/select-title/?title-id=${selectedTitleId}`, {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/title/set-main-title/${selectedTitleId}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
+        credentials: 'include',
       });
 
       if (response.ok) {
@@ -59,12 +64,12 @@ const TitleList: React.FC<TitleListProps> = ({ titles, setUserInfo }) => {
           <li
             key={index}
             className={`p-2 mb-2 text-white rounded flex items-center cursor-pointer group ${selectedTitle === index ? 'ring-4 ring-yellow-500' : ''}`}
-            style={{ backgroundColor: title.color }}
+            style={{ backgroundColor: title.titleColor }}
             onClick={() => setSelectedTitle(index)}
           >
             <div className="flex w-full items-center space-x-2 ">
               <strong className="flex-none w-[200px]">{title.name}</strong>
-              <span className="flex-grow truncate">{title.description}</span>
+              <span className="flex-grow truncate">{title.achieveCondition}</span>
             </div>
           </li>
         ))}
