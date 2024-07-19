e0f53c752563a6d21b70e499cb1072d0d074d9d5
구자건 <wkrjsdl5632@gmail.com>
2024-07-19 22:33:19 +0900

    Fix:냥같은 무한로딩 버그 수정

--- src/components/mypage/profile/profileCard.tsx
+++ src/components/mypage/profile/profileCard.tsx
@@ -20,16 +20,16 @@ interface Title {
 }
 
 interface ProfileCardProps {
-  userInfo: UserInfo | null;
   mainTitle: Title | null;
 }
 
-const ProfileCard: React.FC<ProfileCardProps> = ({ userInfo, mainTitle }) => {
+const ProfileCard: React.FC<ProfileCardProps> = ({mainTitle }) => {
   const [isNicknameModalOpen, setIsNicknameModalOpen] = useState(false);
   const [isPasswordModalOpen, setIsPasswordModalOpen] = useState(false);
   const [isSignOutModalOpen, setIsSignOutModalOpen] = useState(false);
+  const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
   const logout = useAuthStore((state) => state.logout);
-
+  const {username, email} = useAuthStore();
   const router = useRouter()
   const handleLogout = () => {
     logout();
@@ -51,10 +51,6 @@ const ProfileCard: React.FC<ProfileCardProps> = ({ userInfo, mainTitle }) => {
     console.log('탈퇴하기');
   };
 
-  if (!userInfo) {
-    return <div>Loading...</div>;
-  }
-
   return (
     <div className="relative box-border flex-1 min-w-[400px] max-w-[700px] bg-[#EEEEEE] border shadow-lg rounded-lg p-6 mb-4">
       <div className=''>
@@ -71,7 +67,7 @@ const ProfileCard: React.FC<ProfileCardProps> = ({ userInfo, mainTitle }) => {
       <div className="w-full bg-[rgba(223,208,179,0.5)] rounded-2xl p-6">
         <div className="font-MangoDdobak text-2xl leading-[40px] text-black mb-4">
           <div className="flex items-center justify-between mb-2">
-            <p><strong>닉네임:</strong> {userInfo.userName}</p>
+            <p><strong>닉네임:</strong> {username}</p>
             <button
               onClick={handleNicknameChange}
               className="w-[190px] h-[51px] bg-[#4C6C73] rounded-lg text-white text-2xl font-MangoDdobak flex items-center justify-center"
@@ -79,7 +75,7 @@ const ProfileCard: React.FC<ProfileCardProps> = ({ userInfo, mainTitle }) => {
               닉네임 변경
             </button>
           </div>
-          <p className="mb-2"><strong>이메일:</strong> {userInfo.userEmail}</p>
+          <p className="mb-2"><strong>이메일:</strong> {email}</p>
           {mainTitle && (
             <p className="mb-2"><strong>칭호:</strong> {mainTitle.name}</p>
           )}
